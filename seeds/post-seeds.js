180c4e10b9087fec69dfe62472350ba72d97ec41
Derek Sutton <dereksutton86@gmail.com>
2023-06-16 12:19:13 -0400

    added post seeds data

---
+++ seeds/post-seeds.js
@@ -0,0 +1,33 @@
+const { Post } = require('../models');
+
+const postData = [
+    {
+        title: "Why I Love Tech",
+        content: "In this post, I will share my love for all things tech...",
+        user_id: 4
+    },
+    {
+        title: "Top 10 Tech Innovations of the Last Decade",
+        content: "In this post, I will list the top 10 tech innovations in the last decade...",
+        user_id: 2
+    },
+    {
+        title: "How To Start A Tech Blog",
+        content: "In this post, I will share some tips on how to start a tech blog...",
+        user_id: 1
+    },
+    {
+        title: "The Future Of Tech",
+        content: "In this post, I will share my predictions for the future of tech...",
+        user_id: 3
+    },
+    {
+        title: "My Favorite Coding Resources",
+        content: "In this post, I will share some of my favorite coding resources...",
+        user_id: 4
+    }
+];
+
+const seedPosts = () => Post.bulkCreate(postData);
+
+module.exports = seedPosts;
