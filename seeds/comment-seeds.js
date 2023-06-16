6fa23756b8ebaf143fdfed35766e18019765c5e5
Derek Sutton <dereksutton86@gmail.com>
2023-06-16 19:32:44 -0400

    added comment seeds data and reconfigured index.js for seeds

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,28 @@
+const { Comment } = require('../models');
+
+const commentsData = [
+    {
+        content: 'Wow, this post really made me think. Thanks for the great read!',
+        user_id: 1,
+        post_id: 1
+    },
+    {
+        content: 'As an expert in the field, I can say your post is spot on. Well done!',
+        user_id: 4,
+        post_id: 1
+    },
+    {
+        content: 'I disagree with your main point, but I appreciate your thoroughness in covering the topic.',
+        user_id: 1,
+        post_id: 4
+    },
+    {
+        content: 'I never thought of it from this perspective. A lot to consider here!',
+        user_id: 2,
+        post_id: 3
+    }
+];
+
+const seedComments = () => Comment.bulkCreate(commentsData);
+
+module.exports = seedComments;
--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,44 @@
-const seedUsers = require('./user-seeds');
-const seedPosts = require('./post-seeds');
+const { userData, User } = require('./user-seeds');
+const { postsData, Post } = require('./post-seeds');
+const { commentsData, Comment } = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
 
 const seedAllData = async () => {
-    await sequelize.sync({ force: true });
-    console.log('\n----- DATABASE SYNCED -----\n');
-    await seedUsers();
-    console.log('\n----- USERS SEEDED -----\n');
-    await seedPosts();
-    console.log('\n----- POSTS SEEDED -----\n');
+    try {
+        await sequelize.sync({ force: true });
+        console.log('\n----- DATABASE SYNCED -----\n');
 
-    process.exit(0);
+        // Wait for users to be created first before creating posts
+        const users = await User.bulkCreate(userData, {
+            individualHooks: true,
+            returning: true,
+        });
+        console.log('\n----- USERS SEEDED -----\n');
+
+        for (const { id } of users) {
+            const userPosts = postsData
+                .filter((post) => post.user_id === id)
+                .map((post) => ({ ...post, user_id: id }));
+
+            await Post.bulkCreate(userPosts);
+        }
+        console.log('\n----- POSTS SEEDED -----\n');
+
+        for (const { id } of users) {
+            const userComments = commentsData
+                .filter((comment) => comment.user_id === id)
+                .map((comment) => ({ ...comment, user_id: id }));
+
+            await Comment.bulkCreate(userComments);
+        }
+        console.log('\n----- COMMENTS SEEDED -----\n');
+
+        process.exit(0);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 };
 
 seedAllData();
