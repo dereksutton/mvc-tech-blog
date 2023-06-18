32ae8bdba9b57764252cad6214f13fede9f9bddf
Derek Sutton <dereksutton86@gmail.com>
2023-06-18 11:31:40 -0400

    edits to handlebars templates, added frontend js for user registration form

---
+++ public/js/register.js
@@ -0,0 +1,23 @@
+const registrationHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-register').value.trim();
+    const password = document.querySelector('#password-register').value.trim();
+
+    if (username && password) {
+        const res = await fetch('/api/user', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (res.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            console.log('Error encountered.');
+        }
+    }
+};
+
+document.querySelector('.register-btn');
+document.addEventListener('click', registrationHandler);
