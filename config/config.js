4ffaef94e4a3ee488379788c0c502acd985e4e6f
Derek Sutton <dereksutton86@gmail.com>
2023-06-27 15:04:34 -0400

    added a config.js file in order to seed Heroku database

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "dialectOptions": {
+      "decimalNumbers": true
+    }
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "dialectOptions": {
+      "decimalNumbers": true
+    }
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql",
+    "dialectOptions": {
+      "decimalNumbers": true
+    }
+  }
+}
